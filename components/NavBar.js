1fad5a73d0dc31b489d4e20ada249f03eddc736d
AdultStudent0803 <mark.clagett@student.uagc.edu>
2026-07-21 19:02:57 -0700

    NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,20 +1,40 @@
+// FILE: src/components/NavBar.js
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function NavBar() {
+  const getCartCount = () => {
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    return cart.reduce((total, item) => total + item.quantity, 0);
+  };
+
+  const [cartCount, setCartCount] = useState(getCartCount());
+
+  useEffect(() => {
+    const updateCount = () => {
+      setCartCount(getCartCount());
+    };
+
+    window.addEventListener('cartUpdated', updateCount);
+
+    return () => {
+      window.removeEventListener('cartUpdated', updateCount);
+    };
+  }, []);
+
   return (
-    <nav className="navbar">
+    <div className="navbar">
       <h2>🎥 StreamList</h2>
 
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/movies">Movies</Link>
         <Link to="/tmdb">TMDB</Link>
-        <Link to="/cart">Cart</Link>
+        <Link to="/cart">Cart ({cartCount})</Link>
         <Link to="/about">About</Link>
       </div>
-    </nav>
+    </div>
   );
 }
 
